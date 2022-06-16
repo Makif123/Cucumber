531cd9ae19c927bf669767c7c889d3c7ada26531
Junior <94207328+Makif123@users.noreply.github.com>
2022-06-16 16:05:36 +0100

    create a report use via the  plugin

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -6,10 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = "html:target/cucumber-report.html",
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitons",
         dryRun = false, // true means that do not run my step definitions but only check if any step is missing snippet.
-        tags = "@employee and not @admin"
+        tags = ""
 
 )
 public class CukesRunner {
