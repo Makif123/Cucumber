c117461588392028028faf76104ea883e4cabad5
Junior <94207328+Makif123@users.noreply.github.com>
2022-06-17 18:22:06 +0100

    Hook

--- src/test/java/com/cydeo/step_definitons/Hooks.java
+++ src/test/java/com/cydeo/step_definitons/Hooks.java
@@ -5,15 +5,20 @@ package com.cydeo.step_definitons;
     */
 
 
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
 public class Hooks {
     // we import from io.cucumber.java not from junit
     @Before
-    public void setupScenario(){
+    public void setupScenario() {
         System.out.println("======Setting up browser using cucumber @Before");
 
     }
 
+    @After
+    public void teardownScenario() {
+        System.out.println("========Closing browser using cucumber @After");
+    }
 
 }
