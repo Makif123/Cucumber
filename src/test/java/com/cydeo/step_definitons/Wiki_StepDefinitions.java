90a044a27b36adc8e775959b05c90bad2d8d342a
Junior <94207328+Makif123@users.noreply.github.com>
2022-06-18 15:41:20 +0100

    second task wikiStepDefinitions

--- src/test/java/com/cydeo/step_definitons/Wiki_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitons/Wiki_StepDefinitions.java
@@ -33,4 +33,9 @@ public class Wiki_StepDefinitions {
         //verify actual title contains string (coming from feature file)
         Assert.assertTrue(Driver.getDriver().getTitle().contains(string));
     }
+
+    @Then("User sees Steve Jobs is in the main header")
+    public void userSeesSteveJobsIsInTheMainHeader() {
+
+    }
 }
