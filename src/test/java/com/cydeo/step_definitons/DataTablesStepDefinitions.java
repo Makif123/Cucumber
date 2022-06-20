b9f39755633fe2a09c6293d163b847ad6d92f51b
Junior <94207328+Makif123@users.noreply.github.com>
2022-06-20 19:44:12 +0100

    webTableLogin.feature

--- src/test/java/com/cydeo/step_definitons/DataTablesStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitons/DataTablesStepDefinitions.java
@@ -1,6 +1,8 @@
 package com.cydeo.step_definitons;
 
 import com.cydeo.pages.WebTableLoginPage;
+import com.cydeo.utilities.Driver;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
@@ -11,6 +13,20 @@ public class DataTablesStepDefinitions {
     public void user_should_see_fruits_i_like(List<String> fruits) {
         System.out.println(fruits);
         System.out.println(fruits.get(1));
+    }
+
+    @Given("User is on the dropdowns page of practice tool")
+    public void user_is_on_the_dropdowns_page_of_practice_tool() {
+        Driver.getDriver().get("https://practice.cydeo.com/dropdown");
+
+    }
+
+    @Then("User should see below info in month dropdown")
+    public void user_should_see_below_info_in_month_dropdown(List<String> expectedMonths) {
+
+        System.out.println(expectedMonths);
+
+
     }
 
 
