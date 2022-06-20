5f041b830533ae7deddf4c9686e8cc8e1e0480eb
Junior <94207328+Makif123@users.noreply.github.com>
2022-06-20 18:08:22 +0100

    webTableLogin.feature

--- src/test/java/com/cydeo/pages/WebTableLoginPage.java
+++ src/test/java/com/cydeo/pages/WebTableLoginPage.java
@@ -5,9 +5,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class WebTable {
-    public WebTable() {
-        PageFactory.initElements(Driver.getDriver(),this);
+public class WebTableLoginPage {
+    public WebTableLoginPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
     }
 
     @FindBy(xpath = "//input[@type='text']")
--- src/test/java/com/cydeo/step_definitons/WebTablesStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitons/WebTablesStepDefinitions.java
@@ -1,34 +1,43 @@
 package com.cydeo.step_definitons;
 
+import com.cydeo.pages.WebTableLoginPage;
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class WebTablesStepDefinitions {
+    WebTableLoginPage webTableLoginPage = new WebTableLoginPage();
+
     @Given("user is on the login page of web table app")
     public void user_is_on_the_login_page_of_web_table_app() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @When("user enters username {string}")
-    public void userEntersUsername(String arg0) {
+        String url = ConfigurationReader.getProperty("webTableUrl");
+        Driver.getDriver().get(url);
     }
 
-    @And("user enters password {string}")
-    public void userEntersPassword(String arg0) {
+    @When("user enters username {string}")
+    public void user_enters_username(String string) {
+        webTableLoginPage.usernameInput.sendKeys(string);
+
+    }
+
+    @When("user enters password {string}")
+    public void user_enters_password(String string) {
+        webTableLoginPage.passwordInput.sendKeys(string);
+
     }
 
     @When("user should see the login button")
     public void user_should_see_the_login_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("user should see url contains orders")
-    public void user_should_see_url_contains_orders() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        webTableLoginPage.loginButton.click();
+
     }
 
+    @Then("user should see url contains orders")
+    public void user_should_see_url_contains_orders() {
+
+    }
 
 }
